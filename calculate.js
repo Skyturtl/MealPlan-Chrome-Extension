f9f60e926735639f27f5e8985296394e379f74ec
Skyturtl <112505166+Skyturtl@users.noreply.github.com>
2024-08-31 17:01:49 -0500

    Updated to v1.3

--- calculate.js
+++ calculate.js
@@ -7,7 +7,7 @@ function countWeekendDays( d0, d1 )
 
 var today = new Date();
 document.getElementById('first').valueAsDate = new Date("Sun Aug 26 2024 00:00:00 GMT-0500 (Central Daylight Time)");
-document.getElementById('last').valueAsDate = new Date("Fri Dec 18 2024 00:00:00 GMT-0500 (Central Daylight Time)");
+document.getElementById('last').valueAsDate = new Date("Fri Dec 6 2024 00:00:00 GMT-0500 (Central Daylight Time)");
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("btn").addEventListener("click", unhide);
@@ -22,50 +22,53 @@ function unhide() {
     var total = 0;
     var x = document.getElementById("wknd").checked;
     var y = document.getElementById("break").checked;
-    //var z = document.getElementById("thanks").checked;
+    var z = document.getElementById("thanks").checked;
     var last_day = document.getElementById('last').valueAsDate;
     var first_day = document.getElementById('first').valueAsDate;
     var diff = Math.floor((Date.parse(last_day) - Date.parse(today)) / 86400000)+2;
     var diff2 = Math.floor((Date.parse(last_day) - Date.parse(first_day)) / 86400000);
-    var weekendLong = countWeekendDays(first_day, last_day);
     var weekendShort = countWeekendDays(today, last_day);
     
     if(document.getElementById("plan").value == "Plat"){
-        total = 2960;
+        total = 3138;
     }
     if(document.getElementById("plan").value == "Gold"){
-        total = 2650;
+        total = 2809;
     }
     if(document.getElementById("plan").value == "Silver"){
-        total = 2167;
+        total = 2297;
     }
     if(document.getElementById("plan").value == "Bronze"){
-        total = 1674;
+        total = 1775;
     }
     if(document.getElementById("plan").value == "Apartment"){
-        total = 767;
+        total = 813;
     }
     if(document.getElementById("plan").value == "Off-Campus"){
-        total = 453;
+        total = 480;
     }
     var total2 = document.getElementById("cur").value;
+    var clude = "included.";
+    if(x){
+        diff+=2;
+        diff-=weekendShort;
+        clude="excluded.";
+    }
     if(y){
+        diff-=4;
         if(x){
             diff+=2;
-            diff-=weekendShort;
         }
-        diff-=9;
     }
-    // if(z){
-    //     if(x){
-    //         diff+=2;
-    //         diff-=weekendShort;
-    //     }
-    //     diff-=5;
-    // }
+    if(z){
+        diff-=3;
+        if(x){
+            diff+=2;
+        }
+    }
     total = total*(diff/diff2);
     document.getElementById('points').innerHTML = "Points: " + Math.round(100*total)/100 + "   <span class = \"tooltip\">ⓘ <span class = \"tooltiptext w0\"> This is what you should be at. Compare with your current points! </span> </span>";
-    document.getElementById('daily').innerHTML = "Meal Points Per Day: " + Math.round(100*total/(diff))/100 + "   <span class = \"tooltip\">ⓘ <span class = \"tooltiptext w1\"> Days left at school: " + diff + ". Break days are excluded. </span> </span>";
-    document.getElementById('cDaily').innerHTML = "Custom Meal Points Per Day: " + Math.round(100*total2/(diff))/100 + "   <span class = \"tooltip\">ⓘ <span class = \"tooltiptext w2\"> This is suggested number if you had extra meal points from last semester. </span> </span>";
+    document.getElementById('daily').innerHTML = "Meal Points Per Day: " + Math.round(100*total/(diff))/100 + "   <span class = \"tooltip\">ⓘ <span class = \"tooltiptext w1\"> Days left at school: " + diff + ". Break days are excluded. <br> Weekends are " + clude + " </span> </span>";
+    document.getElementById('cDaily').innerHTML = "Custom Meal Points Per Day: " + Math.round(100*total2/(diff))/100 + "   <span class = \"tooltip\">ⓘ <span class = \"tooltiptext w2\"> Amount that you can spend per day given your current meal points. <br> If this is lower than the above daily points per day, spend less. If higher, spend more. </span> </span>";
 }
 
